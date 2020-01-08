24645d62330de87e46d1ab1dd3b94c4465f738ef
Stephen Ramsay <rabrandom@gmail.com>
2020-01-08 14:11:55 +0000

    first test passed

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,11 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.getFilmTitles = function () {
+  const result = this.films.map((film) => {
+    return film.title;
+  });
+  return result;
+};
+
 module.exports = Cinema;
